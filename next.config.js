0897884b38219bf8eaeb73373258f607b03a05e3
한태경 <hanskd12345@gmail.com>
2023-11-10 00:49:07 +0900

    Fix: 정적 사이트 배포시 이미지 안보이는 오류 수정

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "assets.website-files.com",
-      },
-      {
-        protocol: "https",
-        hostname: "fastly.picsum.photos",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
